e1e37660c71c01e9938cfd52c443851ccfb6c7c4
Guntars Simanskis <guntars@diamondgraphics.ie>
2020-02-15 17:05:10 +0000

    Fixed dependency pipe

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {router, Router} = require('./dist/Router');
-const pipe = require('./dist/http/pipe');
-const match = require('./dist/http/match');
+const {pipe} = require('./dist/http/pipe');
+const {match} = require('./dist/http/match');
 const {addHeader, jsonHeader, htmlHeader} = require('./dist/http/header');
 const {response, notFound, STREAM_END} = require('./dist/http/response');
 
