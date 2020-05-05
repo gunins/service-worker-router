e9b31818dc1aac0fffe1e09cc810277d677ac955
Guntars Simanskis <guntars@diamondgraphics.ie>
2020-05-05 15:29:38 +0100

    Remove table from header

--- examples/static/index.js
+++ examples/static/index.js
@@ -2,18 +2,18 @@ import http from 'http';
 import {staticServer} from './Static';
 import {NotFoundResponse} from './NotFoundResponse';
 
-const testIfGetMethod =({method}) => method === 'GET' ? Promise.resolve() : Promise.reject();
+const testIfGetMethod = ({method}) => method === 'GET' ? Promise.resolve() : Promise.reject();
 
-export const StaticFilesServer = (req, resp) =>
-    testIfGetMethod(req)
-        .then(() => staticServer(req, resp, {directory: 'files'}))
-        .catch(() => NotFoundResponse(resp));
 
 const port = 8080;
 
-http
-    .createServer(StaticFilesServer)
-    .listen(port, () => {
-        console.log('app listening on port', port);
-    });
+http.createServer((req, resp) => {
+
+    testIfGetMethod(req)
+        .then(() => staticServer(req, resp, {directory: 'files'}))
+        .catch(() => NotFoundResponse(resp))
+
+}).listen(port, () => {
+    console.log('app listening on port', port);
+});
 
